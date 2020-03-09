c08ffe55f4d7dad820d722783f82bea457cfb730
Daniel Kudrin <danielkudrin1@gmail.com>
2020-03-10 00:52:21 +0300

    add plurality

---
+++ examples/week3/numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int numbers[] = {4, 8, 15, 16, 23, 42};
+    for (int i = 0; i < 6; i++)
+    {
+        if(numbers[i] == 50)
+        {
+            printf("Found.\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
